3af481db5ca632c4501566c9ea3d384fddf86aa9
chineseYouth <revolution202781@outlook.com>
2025-11-04 23:51:03 +0800

    CommandListManager

---
+++ 3-CommandListManager/CommandAllocatorPool.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <stdint.h>
+
+class CommandAllocatorPool
+{
+public:
+    CommandAllocatorPool(D3D12_COMMAND_LIST_TYPE Type);
+    ~CommandAllocatorPool();
+
+    void Create(ID3D12Device* pDevice);
+    void Shutdown();
+
+    ID3D12CommandAllocator* RequestAllocator(uint64_t CompletedFenceValue);
+    void DiscardAllocator(uint64_t FenceValue, ID3D12CommandAllocator* Allocator);
+
+    inline size_t Size() { return mAllocatorPool.size(); }
+
+private:
+
+    const D3D12_COMMAND_LIST_TYPE mCommandListType;                           
+
+    ID3D12Device* mDevice;       
+    std::vector<ID3D12CommandAllocator*> mAllocatorPool;         
+    std::queue<std::pair<uint64_t, ID3D12CommandAllocator*>> mReadyAllocators;  
+    std::mutex mAllocatorMutex;                                                 
+};
